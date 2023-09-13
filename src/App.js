38b86992014fab5d1278596f76ff95a78eb9af1a
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-09-13 10:46:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState("");
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("USD");
+  const [final, setFinal] = useState("");
+
+  useEffect(
+    function () {
+      async function getConversion() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${input}&from=${from}&to=${to}`
+        );
+
+        const data = await res.json();
+        setFinal(data.rates[to]);
+        console.log(data.rates[to]);
+      }
+
+      if (to === from) {
+        setFinal(input);
+        return;
+      }
+      getConversion();
+    },
+    [input, from, to]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={to} onChange={(e) => setTo(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{final} {to}</p>
     </div>
   );
 }
-
-export default App;
